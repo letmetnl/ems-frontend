e1274b4000b0e885b8c9d9e507a5584e921bd538
mohits Projects <mohit5150515@gmail.com>
2026-06-12 16:43:20 +0530

    added browser router using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import "./App.css";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponenet";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          {/* //Route for base url : http://localhost:8080/ */}
+          <Route path="/" element={<ListEmployeeComponent />}></Route>
+          {/* // Route for /employees */}
+          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   );
 }
