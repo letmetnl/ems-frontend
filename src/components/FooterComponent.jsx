a308835f99fe44fd0d6e7a61e4aeafa6670d3e0a
mohits Projects <mohit5150515@gmail.com>
2026-06-12 15:37:46 +0530

    added header and footer components

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <span>@Copyrighted : All Rights reserved - EMS 2026</span>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterComponent;
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const HeaderComponent = () => {
+  return (
+    <div id="header">
+      <header>
+        <nav className="navbar navbar-dark bg-primary justify-content-center">
+          <a className="navbar-brand mx-auto" href="#">
+            Employee Management System
+          </a>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default HeaderComponent;
